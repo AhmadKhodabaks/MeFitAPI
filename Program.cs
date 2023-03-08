17eb97f8148629c602f1b993dfc108ac3d666d84
M.A.R. Khodabaks <rzaa_@live.nl>
2023-03-08 20:52:29 +0100

    test(database): attempt to hide connection string using environmental variables

--- Program.cs
+++ Program.cs
@@ -6,8 +6,24 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Retrieve database credentials from environment variables
+string host = Environment.GetEnvironmentVariable("PGHOST");
+string database = Environment.GetEnvironmentVariable("PGDATABASE");
+string username = Environment.GetEnvironmentVariable("PGUSER");
+string password = Environment.GetEnvironmentVariable("PGPASSWORD");
+string port = Environment.GetEnvironmentVariable("PGPORT");
+
+// Replace placeholders in appsettings.json with actual values
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    .Replace("{PGHOST}", host)
+    .Replace("{PGDATABASE}", database)
+    .Replace("{PGUSER}", username)
+    .Replace("{PGPASSWORD}", password)
+    .Replace("{PGPORT}", port);
+
+
 builder.Services.AddDbContext<MeFitDbContext>(options =>
-                options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+                options.UseNpgsql(connectionString));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
