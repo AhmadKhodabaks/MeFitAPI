c821f754795b8dea8abae83eca0e99826c830269
M.A.R. Khodabaks <rzaa_@live.nl>
2023-03-08 22:48:28 +0100

    feat(exercise): add name property to Exercise class

--- Models/Domain/Exercise.cs
+++ Models/Domain/Exercise.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string MyProperty { get; set; }
+        public string Description { get; set; }
         public string TargetMuscleGroup { get; set; }
         public string ImageLink { get; set; }
         public string VideoLink { get; set; }
