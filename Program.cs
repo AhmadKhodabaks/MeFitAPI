7eaa4e904ad6ae63293717ecc118ab2ae698d6ad
M.A.R. Khodabaks <rzaa_@live.nl>
2023-03-09 02:30:00 +0100

    refactor(api): move code to class, add localhost Swagger option

--- Program.cs
+++ Program.cs
@@ -7,20 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Retrieves database credentials from environment variables
-var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-var databaseUri = new Uri(databaseUrl);
-var userInfo = databaseUri.UserInfo.Split(':');
-var constructor = new NpgsqlConnectionStringBuilder
-{
-    Host = databaseUri.Host,
-    Port = databaseUri.Port,
-    Username = userInfo[0],
-    Password = userInfo[1],
-    Database = databaseUri.LocalPath.TrimStart('/'),
-};
-string connectionString = constructor.ToString();
 
+string connectionString = ConnectionHelper.GetConnectionString(builder.Configuration);
 builder.Services.AddDbContext<MeFitDbContext>(options =>
                 options.UseNpgsql(connectionString));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -30,13 +18,18 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//}
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+app.UseSwagger();
     app.UseSwaggerUI(options => {
         options.SwaggerEndpoint("swagger/v1/swagger.json", "MeFitAPIV1");
         options.RoutePrefix = String.Empty; });
+}
 
 app.UseHttpsRedirection();
 
